75e56a305c638d7cdea03e789245bfb689515b3a
Coen Arrow <coen.arrow@gmail.com>
2024-11-25 10:59:22 +0800

    updated the mtts_can similarly to hr_cnn, fixing imports, and giving options for filtering inputs

--- pyVHR/deepRPPG/mtts_can.py
+++ pyVHR/deepRPPG/mtts_can.py
@@ -1,4 +1,3 @@
-import pyVHR
 import numpy as np
 import tensorflow as tf
 from .MTTS_CAN.model import Attention_mask, MTTS_CAN
@@ -8,6 +7,8 @@ from skimage.util import img_as_float
 import scipy.io
 from scipy.sparse import spdiags
 import h5py
+import os
+import requests
 
 def preprocess_raw_video(frames, fs=30, dim=36):
   """A slightly different version from the original: 
@@ -28,9 +29,6 @@ def preprocess_raw_video(frames, fs=30, dim=36):
     vidLxL[vidLxL < (1/255)] = 1/255
     Xsub[i, :, :, :] = vidLxL
     i = i + 1
-  #import matplotlib.pyplot as plt
-  #plt.imshow(Xsub[0])
-  #plt.show()
 
   # Normalized Frames in the motion branch
   normalized_len = len(t) - 1
@@ -77,18 +75,50 @@ def detrend(signal, Lambda):
     filtered_signal = np.dot((H - np.linalg.inv(H + (Lambda ** 2) * np.dot(D.T, D))), signal)
     return filtered_signal
 
-def MTTS_CAN_deep(frames, fs, model_checkpoint=None, batch_size=100, dim=36, img_rows=36, img_cols=36, frame_depth=10, verb=0, filter_pred=False):
-
+def MTTS_CAN_deep(frames:list, 
+                  fs:float, 
+                  model_checkpoint:str=None, 
+                  batch_size:int=100, 
+                  dim:int=36, 
+                  img_rows:int=36, 
+                  img_cols:int=36, 
+                  frame_depth:int=10, 
+                  verb:int=0):
+  """
+  This function applies the MTTS-CAN model to a video sequence to estimate the heart rate.
+  
+  Parameters
+  ----------
+  frames : list
+      The list of frames of the video sequence.
+  fs : float
+      The frame rate of the video sequence.
+  model_checkpoint : str, optional
+      The path to the pretrained model checkpoint. If None, the model will be downloaded from the repository.
+  batch_size : int, optional
+      The batch size for the model.
+  dim : int, optional
+      The dimension of the frames.
+  img_rows : int, optional
+      The number of rows of the frames.
+  img_cols : int, optional
+      The number of columns of the frames.
+  frame_depth : int, optional
+      How many frames to use for each prediction.
+  verb : int, optional
+      The verbosity of the model.
+  """
   gpus = tf.config.list_physical_devices('GPU')
   if gpus:
      for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu,True)
 
   if model_checkpoint is None:
-    model_checkpoint = pyVHR.__path__[0] +  '/deepRPPG/MTTS_CAN/mtts_can_model.hdf5'
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+    model_checkpoint = os.path.join(module_dir, 'mtts_can_model.hdf5')
     if not os.path.isfile(model_checkpoint):
       url = "https://github.com/phuselab/pyVHR/raw/master/resources/deepRPPG/mtts_can_model.hdf5"
-      print('Downloading MTTS_CAN model...')
+      print(f'Downloading MTTS_CAN model to {model_checkpoint}...')
       r = requests.get(url, allow_redirects=True)
       open(model_checkpoint, 'wb').write(r.content)   
 
@@ -105,11 +135,35 @@ def MTTS_CAN_deep(frames, fs, model_checkpoint=None, batch_size=100, dim=36, img
   yptest = model.predict((dXsub[:, :, :, :3], dXsub[:, :, :, -3:]), batch_size=batch_size, verbose=verb)
 
   # filtering
-  
   pulse_pred = yptest[0]
   pulse_pred = detrend(np.cumsum(pulse_pred), 100)
-  if filter_pred:
-    [b_pulse, a_pulse] = butter(1, [0.75 / fs * 2, 2.5 / fs * 2], btype='bandpass')
-    #[b_pulse, a_pulse] = butter(1, [0.65 / fs * 2, 2.5 / fs * 4], btype='bandpass')
-    pulse_pred = scipy.signal.filtfilt(b_pulse, a_pulse, np.double(pulse_pred))
   return pulse_pred
+
+def filter_MTTS_CAN_outputs(raw_bvp:np.ndarray,
+                            fps:float,
+                            min_bpm:int,
+                            max_bpm:int,
+                            order:int=6) -> np.ndarray:
+    """
+    This function filters the outputs of the MTTS-CAN model.
+    
+    Parameters
+    ----------
+    bvp_outputs : np.ndarray
+        The raw BVP outputs of the model.
+    fps : float
+        The frame rate of the video sequence.
+    min_bpm : int
+        The minimum BPM for the filter.
+    max_bpm : int
+        The maximum BPM for the filter.
+    order : int, optional
+        The order of the butterworth bandpass filter.
+    """
+    low_cutoff = min_bpm / 60
+    high_cutoff = max_bpm / 60
+
+    # filter the outputs
+    [b_pulse, a_pulse] = butter(order, [low_cutoff / fps * 2, high_cutoff / fps * 2], btype='bandpass')
+    filtered_bvp = scipy.signal.filtfilt(b_pulse, a_pulse, np.double(raw_bvp))
+    return filtered_bvp
